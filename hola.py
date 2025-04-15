adf6984f7fc582fcedd73367f9f69b00758ad01a
Rest-999 <Benjamin.santana.a26@gmail.com>
2025-04-15 15:43:55 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,26 @@
+
+#  declaracion de variables
+# nombre="isagi"
+# edad=19
+# ejemplo de concatenacion
+# print("hola", nombre, "y su edad es", edad)
+
+
+
+
+# print("ingrese el nombre")
+# nombre=input()
+# print("ingrese la edad")
+# edad=input()
+
+# print("hola", nombre, "y su edad es", edad)
+
+
+n1=int(input("ingrese un numero "))
+n2=int(input("ingrese un numero "))
+print(n1*n2)
+
+print("el resultado es", n1*n2)
+
+
+
