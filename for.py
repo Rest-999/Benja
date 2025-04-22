5632d09695a6937e50e849de474682377d79a703
Rest-999 <Benjamin.santana.a26@gmail.com>
2025-04-22 16:28:18 -0400

    ejercicios 22 de abril

---
+++ for.py
@@ -0,0 +1,22 @@
+cant=int(print("ingrese la cantidad de notas"))
+
+
+
+for 1 in range(cant):
+    print("ingrese la nota ", 1+1)
+    nota=float(input())
+    total=total+nota
+prom=total/cant
+
+print()
+
+
+
+
+
+
+
+
+
+
+
